5b04b73e75d90f3db508d0db8fd961431431201f
nicki <nicki@DESKTOP-SNURD1J>
2022-11-29 02:20:20 +0300

    Добавьте файлы проекта.

---
+++ Stroitelstvo/ClassFolder/VariableClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroitelstvo.ClassFolder
+{
+    internal class VariableClass
+    {
+        public static int UserId { get; set; }
+        public static string IdRole { get; set; }
+    }
+}
